cc9f24e4f1f16ef7e8c198f723bbf80252d61c78
LaCobraElegante <ianfrancisco2002@gmail.com>
2022-10-11 00:36:43 -0300

    temp fix products

--- js/carrito.js
+++ js/carrito.js
@@ -1,20 +1,106 @@
 //variables
-let allContainerCart = document.querySelector('.productos');
-let containerBuyCart = document.querySelector('.card-items');
 let priceTotal = document.querySelector('.price-total')
 let amountProduct = document.querySelector('.count-product');
-
 
 let buyThings = [];
 let totalCard = 0;
 let countProduct = 0;
 
 //functions
-loadEventListenrs();
-function loadEventListenrs(){
-    allContainerCart.addEventListener('click', addProduct);
 
-    containerBuyCart.addEventListener('click', deleteProduct);
+document.onreadystatechange = () =>{
+    if(document.readyState === "complete"){
+        loadEventListenrs();
+        loadProducts();
+    }
+};
+
+function loadProducts(){
+    productos = [{
+        "id": 1,
+        "nombre": "mesa de comedor",
+        "descripcion": "mesa generica",
+        "precio": 15000,
+        "img": "assets/img/Productos/mesa1.jpg"
+     },
+     {
+        "id": 2,
+        "nombre": "mesa de comedor",
+        "descripcion": "mesa generica",
+        "precio": 9000,
+        "img": "assets/img/Productos/mesa4.jpg"
+     },
+     {
+        "id": 3,
+        "nombre": "silla para patio",
+        "descripcion": "silla",
+        "precio": 6000,
+        "img": "assets/img/Productos/silla5.jpg"
+     },
+     {
+        "id": 4,
+        "nombre": "soporte impresora",
+        "descripcion": "de la mejor calidad!",
+        "precio": 4000,
+        "img": "assets/img/Productos/soporteimpresora.jpg"
+     },
+     {
+        "id": 5,
+        "nombre": "Encimera de cocina",
+        "descripcion": "mesa generica",
+        "precio": 20000,
+        "img": "assets/img/Productos/escritorio11.jpg"
+     },
+     {
+        "id": 6,
+        "nombre": "Silla para living",
+        "descripcion": "silla especial",
+        "precio": 9000,
+        "img": "assets/img/Productos/silla6.jpg"
+     },
+     {
+        "id": 7,
+        "nombre": "Repisa blanca",
+        "descripcion": "blanca y lisa",
+        "precio": 7000,
+        "img": "assets/img/Productos/repisa7.jpg"
+     },
+     {
+        "id": 8,
+        "nombre": "Escritorio",
+        "descripcion": "hola",
+        "precio": 10000,
+        "img": "assets/img/Productos/escritorio1.jpg"
+     },
+     {
+        "id": 9,
+        "nombre": "Sila para patio",
+        "descripcion": "chau",
+        "precio": 6000,
+        "img": "assets/img/Productos/silla5.jpg"
+     }
+    ];
+
+    productos.forEach(function(prod){
+
+        const cuerpo = document.createElement("div");
+        cuerpo.setAttribute("class", "cuerpo");
+        cuerpo.innerHTML = `<img src=${prod.img} alt=${prod.alt}>`;
+        const info = document.createElement("div");
+        info.innerHTML =
+        `<p class="precioProducto">$${prod.precio}</p>
+         <p class="nombreProducto">${prod.nombre}</p>
+         <a href=""><i class="fa-solid fa-cart-plus"></i>Añadir al carrito</a>`;
+        const tarjeta = document.createElement("div");
+        tarjeta.setAttribute("class", "tarjeta");
+        tarjeta.appendChild(cuerpo);
+        tarjeta.appendChild(info);
+        document.querySelector(".productos").appendChild(tarjeta);
+    });
+}
+
+function loadEventListenrs(){
+    document.querySelector('.productos').addEventListener('click', addProduct);
 }
 
 function addProduct(e){
